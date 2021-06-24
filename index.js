27cee3faba47cc0065683ebd89499fdabc39e578
Storm <storm.sarracino01@gmail.com>
2021-06-24 16:01:51 -0500

    commit tree 1c9f6cd4d55d1b10e0cd00f4f28a1e67dd38c1b4

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    const words = sentence.split(' ');
+    
+    const upperCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return upperCaseWords.join(' ');
+  });
 }
